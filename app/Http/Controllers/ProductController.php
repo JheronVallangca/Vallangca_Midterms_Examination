7d840511f7348451a91df16e34056c6da37f81cb
Jheron Vallangca <jdv1104@students.uc-bcf.edu.ph>
2024-06-20 10:53:15 +0800

    Final: Polished codes and Displayed products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function gadgets(){
+        $gadget = [
+        'phone' => 'Infinix Note 12g',
+        'console' => 'Play Station 5',
+        'laptops' => 'Asus Zenbook 14 OLED',
+        'tablet' => 'Cherry Mobile Superion Tab Pro',
+        'apple' => 'Ipad' ];
+        
+        return view('gadget', ['data' => $gadget]);
+}
 }
--- resources/views/gadget.blade.php
+++ resources/views/gadget.blade.php
@@ -1,3 +1,34 @@
-<div>
-    <!-- I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gadgets</title>
+    <style>
+        div{
+            border: 1px solid white; 
+        }
+        body{
+            text-align: center;
+            justify-content: center;
+            background-color: black;
+            color: white;
+        }
+
+    </style>
+    
+</head>
+
+<body>
+    <section class='info'>
+        <div>
+            <h1>Gadgets</h1>
+            <p><strong>Phone:</strong> {{ $data['phone'] }}</p>
+            <p><strong>Console:</strong> {{ $data['console'] }}</p>
+            <p><strong>Laptops:</strong> {{ $data['laptops'] }}</p>
+            <p><strong>Tablet:</strong> {{ $data['tablet'] }}</p>
+            <p><strong>Apple:</strong> {{ $data['apple'] }}</p>
+        </div> 
+     </section>
+</body>
+</html>
